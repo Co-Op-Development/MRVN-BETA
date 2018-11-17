ab633cc63b7dbb05dd7136fff6422b110d98f75e
IttimYT <coopdevelopmentteam@gmail.com>
2018-11-17 16:01:28 -0600

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -3,19 +3,19 @@ exports.run = async (client, message) => {
     .setColor('ORANGE')
     .setTitle("Uptime")
     .setDescription(`${Math.floor(client.uptime / 86400000)}d ${Math.floor(client.uptime / 3600000) % 24}h ${Math.floor(client.uptime / 60000) % 60}m ${Math.floor(client.uptime / 1000) % 60}s`, true)
-    .setFooter(`${message.author.username}`, message.author.displayAvatarURL({format:'png',size:1024})) 
+    .setFooter(`${message.author.username}`, message.author.displayAvatarURL({format:'png',size:1024}))
     .setTimestamp()
-  
+
   message.channel.send(embed)
 }
 
 exports.help = {
   name: 'uptime',
   description: 'Shows the current total uptime of the bot.',
-  usage: 'None',
+  usage: '{}up',
   inHelp: 'yes'
 }
 
 exports.conf = {
-  aliases: []
+  aliases: ['up']
 }
