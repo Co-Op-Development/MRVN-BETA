9c477a19a0d640bd0d63d62b18033a095d09a4f4
IttimYT <coopdevelopmentteam@gmail.com>
2018-11-17 16:16:19 -0600

    Update reboot.js

--- commands/reboot.js
+++ commands/reboot.js
@@ -3,22 +3,22 @@ exports.run = async (client, message) => {
      .setColor('GREEN')
      .setTitle("Reboot")
      .setDescription("Sorry, the `reboot` command can only be executed by the Developer.")
-    .setFooter(`${message.author.username}`, message.author.displayAvatarURL({format:'png',size:1024})) 
+    .setFooter(`${message.author.username}`, message.author.displayAvatarURL({format:'png',size:1024}))
      .setTimestamp()
-    
+
      if (!["342796453477089281", "183790855813988353", "257413019787722752"].includes(message.author.id)) return message.channel.send(NotDev);
-  
+
   let embed = new client.Discord.MessageEmbed()
   .setColor('GREEN')
   .setTitle("Restarting, Please Wait...")
   .addField("Wait a few seconds before running any command.", `\u200b`)
-    .setFooter(`${message.author.username}`, message.author.displayAvatarURL({format:'png',size:1024}))  
+    .setFooter(`${message.author.username}`, message.author.displayAvatarURL({format:'png',size:1024}))
   .setTimestamp()
-  
+
   await message.channel.send(embed)
-  
+
   process.exit(1);
-}; 
+};
 
 exports.help = {
   name: 'reboot',
