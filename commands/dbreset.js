fce61bcfea35f006204c10e12d456437d1af66f5
IttimYT <coopdevelopmentteam@gmail.com>
2018-11-17 16:16:53 -0600

    Update dbreset.js

--- commands/dbreset.js
+++ commands/dbreset.js
@@ -3,9 +3,9 @@ exports.run = async (client, message) => {
      .setColor('GREEN')
      .setTitle("Database Reset")
      .setDescription("Sorry, the `resetDB` command can only be executed by the Developer.")
-         .setFooter(`${message.author.username}`, message.author.displayAvatarURL({format:'png',size:1024})) 
+         .setFooter(`${message.author.username}`, message.author.displayAvatarURL({format:'png',size:1024}))
      .setTimestamp()
-  
+
   if (!["342796453477089281", "183790855813988353"].includes(message.author.id)) return message.channel.send(NotDev);
   client.userInfo.deleteAll()
   return message.channel.send(`User database has successfully been reset!`);
