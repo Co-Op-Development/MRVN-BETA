3d7ffccaf3d371a29337560fab995f6bca453024
IttimYT <coopdevelopmentteam@gmail.com>
2018-11-17 16:10:56 -0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -3,12 +3,13 @@ module.exports = {
     const experienceToNextLevel = level * 1000 + 125;
     const experienceGained = Math.floor(Math.random() * 7) + 5;
     const userInfo = client.userInfo.get(id);
-    
+
     if (experienceGained + userInfo.exp > experienceToNextLevel) {
       client.userInfo.setProp(id, "level", level + 1);
     } else {
       client.userInfo.setProp(id, "exp", userInfo.exp + experienceGained);
     }
- 
+
   }
 }
+
