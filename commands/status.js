5207c9d27fd61be58c7031e5d14a8d40fcec688f
IttimYT <coopdevelopmentteam@gmail.com>
2018-11-19 17:52:56 -0600

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,11 +1,11 @@
 exports.run = async (client, message) => {
+  let embed = new client.Discord.MessageEmbed()
  .setColor('ORANGE')
     .setTitle('Status')
     .addField('Name', 'MRVN', true)
-    .addfield() 
+    .addField("API Latency", `${Math.round(client.ping)}ms`, true)
     .addField("Uptime",`${Math.floor(client.uptime / 86400000)}d ${Math.floor(client.uptime / 3600000) % 24}h ${Math.floor(client.uptime / 60000) % 60}m ${Math.floor(client.uptime / 1000) % 60}s`, true)
-    .addField()
-   .addField('Support Server', 'https://discord.gg/fcm7Yh3', true)
+    .addField('Support Server', 'https://discord.gg/fcm7Yh3', true)
     .setFooter(`${message.author.username}`, message.author.displayAvatarURL({format:'png',size:1024}))
     .setTimestamp()
 
