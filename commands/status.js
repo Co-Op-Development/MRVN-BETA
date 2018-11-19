4885727298d4e23f49224ea7abfcf24e01c6a3c8
IttimYT <coopdevelopmentteam@gmail.com>
2018-11-19 17:42:56 -0600

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,7 +1,16 @@
 exports.run = async (client, message) => {
-// Code goes here
+ .setColor('ORANGE')
+    .setTitle('Status')
+    .addField('Name', 'MRVN', true)
+    .addfield() 
+    .addField("Uptime",`${Math.floor(client.uptime / 86400000)}d ${Math.floor(client.uptime / 3600000) % 24}h ${Math.floor(client.uptime / 60000) % 60}m ${Math.floor(client.uptime / 1000) % 60}s`, true)
+    .addField()
+   .addField('Support Server', 'https://discord.gg/fcm7Yh3', true)
+    .setFooter(`${message.author.username}`, message.author.displayAvatarURL({format:'png',size:1024}))
+    .setTimestamp()
+
+  message.channel.send(embed)
 }
-// Name must be lowercase
 exports.help = {
   name: 'status',
   description: 'Shows the status of MRVN',
