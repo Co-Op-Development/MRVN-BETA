62491c00a8e3104b5d7bffa27caf29c0149159b8
IttimYT <coopdevelopmentteam@gmail.com>
2018-11-19 17:40:55 -0600

    Create status.js

---
+++ commands/status.js
@@ -0,0 +1,14 @@
+exports.run = async (client, message) => {
+// Code goes here
+}
+// Name must be lowercase
+exports.help = {
+  name: 'status',
+  description: 'Shows the status of MRVN',
+  usage: 'None',
+  inHelp: 'Yes'
+}
+
+exports.conf = {
+  aliases: []
+}
