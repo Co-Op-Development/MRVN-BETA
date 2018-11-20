aa7181464be77e86c675c810f37339e6451157d2
IneedMoarCake <admin@arcanebot.xyz>
2018-11-19 20:29:58 -0800

    OCD ;/

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,24 +1,25 @@
 exports.run = async (client, message) => {
+  
   let data = client.userInfo.array();
   let d = data.sort((a, b) => b.level - a.level).splice(0, 10);
-
   var sending = "";
+  
   for (var i = 0; i < d.length; i++) {
-
     let user = client.users.get(d[i].id);
     if (!user) return;
     sending += `\`${i+1}\`. ${user.username}, level **${d[i].level}** with **${d[i].exp}** exp\n`
   }; 
   
   const embed = new client.Discord.MessageEmbed()
-    .setColor('ORANGE')
-    .addField("Global Level Leaderboard", sending)
-    .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL({format:'png',size:1024}))
-    .setTimestamp()
+  .setColor('ORANGE')
+  .addField("Global Level Leaderboard", sending)
+  .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL({format:'png',size:1024}))
+  .setTimestamp()
 
-     message.channel.send(embed)
+  message.channel.send(embed)
 
 }
+
 exports.help = {
   name: 'xpleaderboard',
   description: 'Shows up to the top 10 users and their level',
